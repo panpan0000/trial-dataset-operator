1e3cfd53b1f51d025bec1cbd2f42b761bbc57b33
Peter Pan <Peter.Pan@daocloud.io>
2023-11-28 06:14:07 -0500

    logic code

--- api/v1/dataset_types.go
+++ api/v1/dataset_types.go
@@ -17,11 +17,46 @@ limitations under the License.
 package v1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// DataVolumeSourceHTTP can be either an http or https endpoint, with an optional basic auth user name and password, and an optional configmap containing additional CAs
+type DataVolumeSourceHTTP struct {
+	// URL is the URL of the http(s) endpoint
+	URL string `json:"url"`
+	// SecretRef A Secret reference, the secret should contain accessKeyId (user name) base64 encoded, and secretKey (password) also base64 encoded
+	// +optional
+	SecretRef string `json:"secretRef,omitempty"`
+	// CertConfigMap is a configmap reference, containing a Certificate Authority(CA) public key, and a base64 encoded pem certificate
+	// +optional
+	CertConfigMap string `json:"certConfigMap,omitempty"`
+	// ExtraHeaders is a list of strings containing extra headers to include with HTTP transfer requests
+	// +optional
+	ExtraHeaders []string `json:"extraHeaders,omitempty"`
+	// SecretExtraHeaders is a list of Secret references, each containing an extra HTTP header that may include sensitive information
+	// +optional
+	SecretExtraHeaders []string `json:"secretExtraHeaders,omitempty"`
+}
+
+// DataVolumeBlankImage provides the parameters to create a new raw blank image for the PVC
+type DataVolumeBlankImage struct{}
+
+// DataVolumeSource represents the source for our Data Volume, this can be HTTP, Imageio, S3, GCS, Registry or an existing PVC
+type DataVolumeSource struct {
+	HTTP *DataVolumeSourceHTTP `json:"http,omitempty"`
+	/*	NFS      *DataVolumeSourceNFS      `json:"nfs,omitempty"`
+		S3       *DataVolumeSourceS3       `json:"s3,omitempty"`
+		GCS      *DataVolumeSourceGCS      `json:"gcs,omitempty"`
+		Registry *DataVolumeSourceRegistry `json:"registry,omitempty"`
+		PVC      *DataVolumeSourcePVC      `json:"pvc,omitempty"`
+		Upload   *DataVolumeSourceUpload   `json:"upload,omitempty"`
+	*/
+	Blank *DataVolumeBlankImage `json:"blank,omitempty"`
+}
 
 // DatasetSpec defines the desired state of Dataset
 type DatasetSpec struct {
@@ -29,18 +64,27 @@ type DatasetSpec struct {
 	// Important: Run "make" to regenerate code after modifying this file
 
 	// Foo is an example field of Dataset. Edit dataset_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Foo    string            `json:"foo,omitempty"`
+	Source *DataVolumeSource `json:"source,omitempty"`
+	//SourceRef is an indirect reference to the source of data for the requested DataVolume
+	//PVC is the PVC specification
+	PVC *corev1.PersistentVolumeClaimSpec `json:"pvc,omitempty"`
 }
 
 // DatasetStatus defines the observed state of Dataset
 type DatasetStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	Phase    string `json:"phase,omitempty"`
+	Progress string `json:"progress,omitempty"`
 }
 
-//+kubebuilder:object:root=true
-//+kubebuilder:subresource:status
-
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Phase",type="string",JSONPath=".status.phase",description="The phase the data volume is in"
+// +kubebuilder:printcolumn:name="Progress",type="string",JSONPath=".status.progress",description="Transfer progress in percentage if known, N/A otherwise"
+// +kubebuilder:printcolumn:name="Restarts",type="integer",JSONPath=".status.restartCount",description="The number of times the transfer has been restarted."
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 // Dataset is the Schema for the datasets API
 type Dataset struct {
 	metav1.TypeMeta   `json:",inline"`
--- internal/controller/dataset_controller.go
+++ internal/controller/dataset_controller.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,19 +18,113 @@ package controller
 
 import (
 	"context"
+	"fmt"
 
+	datasetopsv1 "dataset-operator/api/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	datasetopsv1 "dataset-operator/api/v1"
 )
 
 // DatasetReconciler reconciles a Dataset object
 type DatasetReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+}
+
+// /////////////////////////////////
+func createImporterPod(ctx context.Context, namespacedName types.NamespacedName, dataset *datasetopsv1.Dataset) error {
+	clientset, err := newKubeClient()
+	if err != nil {
+		return err
+	}
+	httpURL := dataset.Spec.Source.HTTP.URL
+	pvcName := namespacedName.Name
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "importer-" + namespacedName.Name,
+			Namespace: namespacedName.Namespace,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  "importer",
+					Image: "nginx",
+					Command: []string{
+						"/bin/sh",
+						"-c",
+						"curl " + httpURL + " -Lo /mnt/a.out",
+					},
+					VolumeMounts: []corev1.VolumeMount{
+						{
+							Name:      "my-volume",
+							MountPath: "/mnt",
+						},
+					},
+				},
+			},
+			Volumes: []corev1.Volume{
+				{
+					Name: "my-volume",
+					VolumeSource: corev1.VolumeSource{
+						PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+							ClaimName: pvcName,
+						},
+					},
+				},
+			},
+		},
+	}
+	createdPod, err := clientset.CoreV1().Pods(namespacedName.Namespace).Create(ctx, pod, v1.CreateOptions{})
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Pod %s created\n", createdPod.Name)
+	return nil
+}
+
+// ///////////////////////////
+func createPVC(ctx context.Context, namespacedName types.NamespacedName, pvcSpec *corev1.PersistentVolumeClaimSpec) error {
+	clientset, err := newKubeClient()
+	if err != nil {
+		return err
+	}
+
+	// Create a PersistentVolumeClaim object
+	pvc := &corev1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      namespacedName.Name,
+			Namespace: namespacedName.Namespace,
+		},
+		Spec: *pvcSpec,
+	}
+	createdPVC, err := clientset.CoreV1().PersistentVolumeClaims(namespacedName.Namespace).Create(ctx, pvc, v1.CreateOptions{})
+	fmt.Printf("DEBUG, createdPVC=%s", createdPVC)
+	return err
+}
+
+// / FIXME .. should be new once..
+// should get from /var/run/secret serviceAccount token & ca.crt
+func newKubeClient() (*kubernetes.Clientset, error) {
+	kubeC := "/root/.kube/config"
+	config, err := clientcmd.BuildConfigFromFlags("", kubeC)
+	if err != nil {
+		return nil, err
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return clientset, nil
 }
 
 //+kubebuilder:rbac:groups=dataset-ops.my.domain,resources=datasets,verbs=get;list;watch;create;update;patch;delete
@@ -48,9 +142,31 @@ type DatasetReconciler struct {
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
 func (r *DatasetReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
-
+	dataset := &datasetopsv1.Dataset{}
+	if err := r.Get(ctx, req.NamespacedName, dataset); err != nil {
+		return ctrl.Result{Requeue: true}, err
+	}
+	if dataset.Status.Phase == "Completed" {
+		return ctrl.Result{}, nil
+	}
+	dataset.Status.Phase = "Processing"
+	if err := r.Status().Update(ctx, dataset); err != nil {
+		return ctrl.Result{}, err
+	}
 	// TODO(user): your logic here
-
+	err := createPVC(ctx, req.NamespacedName, dataset.Spec.PVC)
+	if err != nil {
+		panic(err.Error())
+	}
+	err = createImporterPod(ctx, req.NamespacedName, dataset)
+	if err != nil {
+		panic(err.Error())
+	}
+	// FIXME, it's just an example, should wait Pod Completed
+	dataset.Status.Phase = "Completed"
+	if err := r.Status().Update(ctx, dataset); err != nil {
+		return ctrl.Result{}, err
+	}
 	return ctrl.Result{}, nil
 }
 
